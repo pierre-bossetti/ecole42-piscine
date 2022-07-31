6f0476f207efe8a825d0e567598b53bfe2c76d10
Pierre <pierrebossetti_348@hotmail.fr>
2022-07-31 08:51:27 +0200

    Correction exercice 0

--- C_06/ex00/ft_print_program_name.c
+++ C_06/ex00/ft_print_program_name.c
@@ -1,11 +1,20 @@
 #include <unistd.h>
 
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
 int main(int argc, char *argv[])
 {
-    int len;
-
-    len = sizeof (argv[0]);
-    write(1, argv[0], len);
+    int i;
+    
+    i = 0;
+    while (argv[0][i] != '\0')
+    {
+        ft_putchar(argv[0][i]);
+        i++;
+    } 
     write(1, "\n", 1);
     return (0);
 }
